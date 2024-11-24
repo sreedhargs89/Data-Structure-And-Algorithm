78787ec04d4a5dad05800da30f0c85bcdb3437e1
Sreedhar GS <sreedhargs120@gmail.com>
2024-11-24 17:11:46 +0530

    string reversal problem and multi stack

---
+++ com/gss/datastructures/ReverseString.java
@@ -0,0 +1,26 @@
+package com.gss.datastructures;
+
+import java.util.Stack;
+
+public class ReverseString {
+
+    public static String reverseString(String input){
+        String output = "";
+        Stack<Character> inputStack = new Stack<>();
+        char[] characters =  input.toCharArray();
+        for (int i = 0; i < characters.length ; i++) {
+            inputStack.push(characters[i]);
+        }
+
+        while (!inputStack.isEmpty()){
+            output = output+inputStack.pop();
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        String input = "abcdefghijklmnopqrstuvxyz";
+        String output = reverseString(input);
+        System.out.println("Reversed String =" + output );
+    }
+}
--- com/gss/datastructures/StackExample.java
+++ com/gss/datastructures/StackExample.java
@@ -1,9 +1,23 @@
 package com.gss.datastructures;
 
+import java.util.Stack;
+
 public class StackExample {
 
     public static void main(String[] args) {
         System.out.println("Hello, World, Stack!");
+
+        String str = new String();
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println(stack);
+        // last added item first out now
+        stack.pop();
+        System.out.println(stack);
 
 
     }
